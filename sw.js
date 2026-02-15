196654ad2bbe46710b033da8b178dedfdfae2125
mor00ai <moralex00ai@gmail.com>
2026-02-15 17:14:58 +0100

    Add files via upload
    caricamento iniziale

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'finanze-pro-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest'
+];
+
+// Installazione Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Attivazione e pulizia vecchia cache
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Strategia: Network first, fallback on cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
